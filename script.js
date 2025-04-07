8adca59b3b891d71bded2a2dcc09dd80d4c391cc
ThamizhAnban <thamizhanban2006@gmail.com>
2025-04-07 10:12:57 +0530

    created jwt and expiration time

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+  
+  const token = jwt.sign(payload, secret, { expiresIn: '1m' });
+  console.log(token);
+  return token;
+  
 };
+
+const payload = {
+  username: 'labuser',
+  role: 'student'
+};
+
+const secret = 'Cat';
+
+encrypt(payload,secret)
+
 
 module.exports = encrypt;
